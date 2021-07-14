f86059d1441a0556ef98c38287cd5afa74274a65
dmitry-glushkov <glushkov.dmitry.fn@yandex.ru>
2021-07-14 22:50:23 +0300

    add storage

---
+++ internal/store/store.go
@@ -0,0 +1,44 @@
+package store
+
+import "time"
+
+type Store struct {
+	Data map[string]interface{}
+}
+
+func New() *Store {
+	return &Store{
+		Data: make(map[string]interface{}),
+	}
+}
+
+// Принимает ключ, значение и время жизни записи, создает запись
+func (s *Store) Set(key string, value interface{}, ttl int) {
+	s.Data[key] = value
+	if ttl != 0 {
+		liveTime := time.Duration(ttl) * time.Second
+		timer := time.AfterFunc(liveTime, func() {
+			delete(s.Data, key)
+		})
+		defer timer.Stop()
+	}
+}
+
+// Возвращает значение для заданного ключа
+func (s *Store) Get(key string) interface{} {
+	return s.Data[key]
+}
+
+// Возвращает список всех существующих ключей
+func (s *Store) GetAllKeys() []string {
+	keys := make([]string, 0, len(s.Data))
+	for k := range s.Data {
+		keys = append(keys, k)
+	}
+	return keys
+}
+
+// Удаляет запись по ключу
+func (s *Store) Delete(key string) {
+	delete(s.Data, key)
+}
